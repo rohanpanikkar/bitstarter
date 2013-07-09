104dbfc9fa4203468a7c4fd1bf4ecb743ceb70eb
Rohan Panikkar <rohanpanikkar@gmail.com>
2013-07-09 13:12:41 -0400

    Reading from index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var fs = require('fs');
     var content = fs.readFileSync('index.html','utf8'); 
-    response.send('Hello World 2!');
+    response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
